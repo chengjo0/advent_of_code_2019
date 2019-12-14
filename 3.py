5ba0a3813440f3552cd05af1d1804321923f0fbb
John Cheng <john.cheng@futurice.com>
2019-12-14 15:43:17 +0200

    Day 3 second part

--- 3.py
+++ 3.py
@@ -55,6 +55,13 @@ if __name__ == "__main__":
     input = request.text.splitlines()
     # wire_1 = Wire("R8,U5,L5,D3".split(','))
     # wire_2 = Wire("U7,R6,D4,L4".split(','))
+
+    # wire_1 = Wire("R75,D30,R83,U83,L12,D49,R71,U7,L72".split(','))
+    # wire_2 = Wire("U62,R66,U55,R34,D71,R55,D58,R83".split(','))
+
+    # wire_1 = Wire("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51".split(','))
+    # wire_2 = Wire("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split(','))
+
     wire_1 = Wire(input[0].split(','))
     wire_2 = Wire(input[1].split(','))
 
@@ -62,17 +69,24 @@ if __name__ == "__main__":
     wire_2_positions = wire_2.get_positions()
 
     common_positions = []
-    print(len(wire_1_positions))
-    print(len(wire_2_positions))
-    index = 0
+    least_steps_taken = 10000000000000000000
+    flag = False
+    # print(len(wire_1_positions))
+    # print(len(wire_2_positions))
     lists = (wire_1_positions, wire_2_positions) if len(wire_1_positions) <= len(
         wire_2_positions) else (wire_2_positions, wire_1_positions)
-    for p1 in lists[0]:
-        print(index)
-        for p2 in lists[1]:
+    for j, p1 in enumerate(lists[0], 1):
+        for k, p2 in enumerate(lists[1], 1):
             if p1 == p2:
                 common_positions.append(p2)
-        index += 1
+                least_steps_taken = min(least_steps_taken, j + k)
+                flag = True
+                print("Steps taken to reach intersection: {}".format(j + k))
+
+        #     if flag:
+        #         break
+        # if flag:
+        #     break
 
     if len(common_positions) > 0:
         closest_position = abs(
@@ -81,6 +95,13 @@ if __name__ == "__main__":
             closest_position = min(closest_position, abs(
                 common_positions[common_position][0]) + abs(common_positions[common_position][1]))
 
-        print(closest_position)
+        print("""
+Result:
+--> Closest position: {}
+--> Least steps taken to reach intersetion: {} steps
+        """.format(closest_position, least_steps_taken))
+        # print("Closest position: {}".format(closest_position))
+        # print("Least steps taken to reach intersetion: {} steps".format(
+        # least_steps_taken))
     else:
         print("No common position...")
